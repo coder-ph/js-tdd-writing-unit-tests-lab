86d2bf62e42ca0124745d58a698f5eb9c6f48c84
Phelix <phelixmbani@gmail.com>
2025-01-04 01:41:30 +0300

    unit test

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,9 @@
 // Your tests here
+import {isPalindrome} from '../utils'
+
+describe('isPalindrome', ()=> {
+    test('isPalindrome returns true if palindrome else false', ()=>{
+        const word = 'racecar'
+        expect(isPalindrome(word)).toBe(true)
+    })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,8 @@
 // Your code here
+export function isPalindrome(word) {
+    const cleanedWord = word.toLowerCase().replace(/[^a-z0-9]/g, '')
+    const newWord = cleanedWord.split('').reverse().join('')
+    return cleanedWord ===newWord
+}
+
+console.log(isPalindrome('racecar'))
